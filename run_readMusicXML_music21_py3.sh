d8ad207ca5b549c4473cd1529a67c87defb8e1f0
cx1964 <cx1964@mail.com>
2021-03-19 15:17:20 +0100

    install pyenv

---
+++ run_readMusicXML_music21_py3.sh
@@ -0,0 +1,5 @@
+# file: run_readMusicXML_music21_py3.sh
+# function: shell script to run van python readMusicXML_music21_py3.py script
+# zie https://medium.com/@autoferrit/how-to-use-pyenv-pipenv-for-python-virtual-environments-fe70459a4037
+
+pipenv run python readMusicXML_music21_py3.py 
