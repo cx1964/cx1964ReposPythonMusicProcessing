eef51343cf73e28358878997df52a5d9b5e2fccf
cx1964 <cx1964@mail.com>
2021-03-19 16:31:17 +0100

    works

--- readMusicXML_music21_py3.py
+++ readMusicXML_music21_py3.py
@@ -8,10 +8,11 @@ import music21 as m
 musescoreProg='MuseScore-3.6.2.548021370-x86_64_461d9f78f967c0640433c95ccb200785.AppImage'
 scorePath = "/home/claude/Documents/sources/python/python3/cx1964ReposPythonMusicProcessing"
 # Export de MuseScore File in musicxml (uncompressed music xml format musicxml extention)
-museScoreFile = "C_major_scale_ascending.MusicXML.MusicXML" # in musicxml uncompressed
+museScoreFile = "C_major_scale_ascending.musicxml" # in musicxml uncompressed
 keyCmin = m.key.Key('C') #  lowercase = c minor. uppercase = C major
 
-# Zie: https://web.mit.edu/music21/doc/usersGuide/usersGuide_24_environment.html#usersguide-24-environment
+# See: https://web.mit.edu/music21/doc/usersGuide/usersGuide_24_environment.html#usersguide-24-environment
+# See: https://web.mit.edu/music21/doc/usersGuide/usersGuide_24_environment.html
 env = m.environment.UserSettings()
 env.delete()
 env.create()
@@ -22,4 +23,4 @@ env['autoDownload'] = 'allow'
 env['musicxmlPath'] = '/home/claude/Applications/'+musescoreProg
 
 myScore = m.converter.parse(scorePath+'/'+museScoreFile, format='musicxml')
-#myScore.show()  
+myScore.show()  
