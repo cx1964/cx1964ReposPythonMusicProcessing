1fc2ea551e0a9bd28b43e0f2f4cd2b71b7ffe665
cx1964 <cx1964@mail.com>
2021-03-19 16:09:13 +0100

    test start musescore AppImage

---
+++ edit_musecore_mscz.sh
@@ -0,0 +1,3 @@
+# File: edit_musescore_mscz.sh
+# Function: run Musescore AppImage with sheetmusic in mscz format
+~/Applications/MuseScore-3.6.2.548021370-x86_64_461d9f78f967c0640433c95ccb200785.AppImage C_major_scale_ascending.mscz
